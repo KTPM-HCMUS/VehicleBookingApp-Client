72601eb897da19ee0997569ba4df8e2c117debf7
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2022-08-10 19:32:09 +0700

    dropoff & driverarrival

--- VehicleBooking/app/src/main/java/com/ktpm/vehiclebooking/activities/SplashActivity.java
+++ VehicleBooking/app/src/main/java/com/ktpm/vehiclebooking/activities/SplashActivity.java
@@ -2,15 +2,32 @@ package com.ktpm.vehiclebooking.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.ktpm.vehiclebooking.R;
 
+/**
+ * Loading screen activity
+ */
 public class SplashActivity extends AppCompatActivity {
 
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
