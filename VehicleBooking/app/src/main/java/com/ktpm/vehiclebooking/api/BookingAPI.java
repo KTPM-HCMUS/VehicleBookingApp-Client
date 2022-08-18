f64f2e2989fa0d5449982749b55d4d4305940ca1
NhanNguyen <64063604+nguyentrongnhan1412@users.noreply.github.com>
2022-08-18 13:49:41 +0700

    BookingAPI

---
+++ VehicleBooking/app/src/main/java/com/ktpm/vehiclebooking/api/BookingAPI.java
@@ -0,0 +1,29 @@
+package com.ktpm.vehiclebooking.api;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.ktpm.vehiclebooking.model.Booking;
+import com.ktpm.vehiclebooking.model.ResponseTT;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface BookingAPI {
+    Gson gson = new GsonBuilder()
+            .setDateFormat("dd-MM-yyyy")
+            .create();
+    com.ktpm.vehiclebooking.api.BookingAPI apiService = new Retrofit.Builder()
+            .baseUrl("http://34.72.120.102:8080/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(com.ktpm.vehiclebooking.api.BookingAPI.class);
+
+    @POST("booking")
+    Call<ResponseTT> login(@Body Booking booking);
+
+
+    @POST("v1/revokeToken")
+    Call<ResponseTT> revokeToken(@Header("Authorization") String Authorization);
+}
